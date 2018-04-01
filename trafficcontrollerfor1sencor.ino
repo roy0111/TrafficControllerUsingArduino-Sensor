7e739fed85f85d877fc04c97f12249abe2672fac
roy0111 <biratewu0111@gmail.com>
2018-04-01 21:47:27 +0600

    Add files via upload
    for 1 sensor

---
+++ trafficcontrollerfor1sencor.ino
@@ -0,0 +1,40 @@
+#define sencor1EchoPin 13
+#define sencor1TrigPin 12
+
+#define GreenLedPin 11
+
+
+void setup() {
+ 
+ Serial.begin(9600);
+ pinMode(sencor1TrigPin, OUTPUT);
+ pinMode(sencor1EchoPin, INPUT);
+
+ pinMode(GreenLedPin,OUTPUT);
+}
+
+void loop() {
+  secsor1Sence();
+}
+
+void secsor1Sence(){
+ long duration, distance;
+ digitalWrite(sencor1TrigPin, LOW);
+ delayMicroseconds(2);
+ digitalWrite(sencor1TrigPin, HIGH);
+ delayMicroseconds(10);
+ digitalWrite(sencor1TrigPin, LOW);
+ duration = pulseIn(sencor1EchoPin, HIGH);
+ distance = (duration/2) / 29.1;
+
+ if ( distance <= 15){
+ digitalWrite(GreenLedPin,HIGH);
+    }
+else{
+ digitalWrite(GreenLedPin,LOW);
+    }
+    Serial.println(" countodd");
+    delay(50);
+    Serial.println(" counteven");
+}
+
